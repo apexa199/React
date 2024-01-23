f3778cff2f03ccadb6c32d18b63f25e3a28ee473
apexa199 <apexaajudiya9@gmail.com>
2024-01-23 13:37:16 +0530

    sentry.io error handling

--- Redux_saga/src/index.js
+++ Redux_saga/src/index.js
@@ -17,6 +17,31 @@ import { ListTutorial } from "./components/ListTutorial";
 import { ErrorBoundary } from "react-error-boundary";
 import BuggyCounterError from "./BuggyCounterError";
 import ErrorHandling from "./ErrorHandling";
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://d9cfead0f2ddcccfd44d0a392cf952d4@o4506619416805376.ingest.sentry.io/4506619465760768",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
